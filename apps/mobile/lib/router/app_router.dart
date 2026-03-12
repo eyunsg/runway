134ad58ec78f82bd930b5cf199930f1187a629fd
eyunsg <iyunsung423@gmail.com>
2026-03-12 16:28:30 +0900

    Feature/RUNWAY-356 비밀번호 재설정 화면 임시 UI 구현 (#43)
    * feat(RUNWAY-356): 비밀번호 재설정 화면 임시 UI 구현
    
    * feat(RUNWAY-356): 비밀번호 재설정 화면 라우트 추가
    
    * feat(RUNWAY-356): 로그인 화면에 비밀번호 초기화 라우트 연결

--- apps/mobile/lib/router/app_router.dart
+++ apps/mobile/lib/router/app_router.dart
@@ -1,6 +1,8 @@
 import 'package:go_router/go_router.dart';
 
 import 'package:runway/features/login/page/login_temp_screen.dart';
+import 'package:runway/features/password_reset/page/email_input_temp_screen.dart';
+import 'package:runway/features/password_reset/page/password_reset_temp_screen.dart';
 import '../features/register/page/register_temp_screen.dart';
 
 class AppRouter {
@@ -12,6 +14,14 @@ class AppRouter {
         builder: (context, state) => RegisterTempScreen(),
       ),
       GoRoute(path: '/login', builder: (context, state) => LoginTempScreen()),
+      GoRoute(
+        path: '/reset-password',
+        builder: (context, state) => EmailInputTempScreen(),
+      ),
+      GoRoute(
+        path: '/reset-password/new',
+        builder: (context, state) => PasswordResetTempScreen(),
+      ),
     ],
   );
 }
