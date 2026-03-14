9a9d7cf328d3c7cef3cfefcbea0355785c9994b3
eyunsg <iyunsung423@gmail.com>
2026-03-15 08:00:17 +0900

    refactor(RUNWAY-312): passwordChangeInput에서 password value object 제거 및 문자열 검증으로 변경

--- apps/mobile/lib/domain/entities/password_change_input.dart
+++ apps/mobile/lib/domain/entities/password_change_input.dart
@@ -1,6 +1,6 @@
 class PasswordChangeInput {
-  final Password currentPassword;
-  final Password newPassword;
+  final String currentPassword;
+  final String newPassword;
   final String newPasswordConfirm;
 
   PasswordChangeInput({
@@ -8,22 +8,16 @@ class PasswordChangeInput {
     required this.newPassword,
     required this.newPasswordConfirm,
   }) {
-    if (newPassword.value != newPasswordConfirm) {
+    if (newPassword.length < 6) {
+      throw ArgumentError('새 비밀번호는 최소 6자 이상이어야 합니다.');
+    }
+
+    if (newPassword != newPasswordConfirm) {
       throw ArgumentError('새 비밀번호가 일치하지 않습니다.');
     }
 
-    if (currentPassword.value == newPassword.value) {
+    if (currentPassword == newPassword) {
       throw ArgumentError('새 비밀번호는 현재 비밀번호와 다르게 설정해야 합니다.');
-    }
-  }
-}
-
-class Password {
-  final String value;
-
-  Password(this.value) {
-    if (value.length < 6) {
-      throw ArgumentError('비밀번호는 최소 6자 이상이어야 합니다.');
     }
   }
 }
