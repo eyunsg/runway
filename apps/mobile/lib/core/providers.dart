a53fcc7997a943357560f74a0a2252557bda5803
eyunsg <iyunsung423@gmail.com>
2026-03-19 22:22:29 +0900

    feat(RUNWAY-323): 프로필 관련 provider 구현

--- apps/mobile/lib/core/providers.dart
+++ apps/mobile/lib/core/providers.dart
@@ -11,6 +11,11 @@ import '../features/login/types/login_state.dart';
 import '../features/login/usecase/login_usecase.dart';
 import '../features/login/repository/login_repository.dart';
 
+import 'package:runway/features/profile/controller/profile_controller.dart';
+import 'package:runway/features/profile/repository/profile_reposity.dart';
+import 'package:runway/features/profile/usecase/profile_usecase.dart';
+import 'package:runway/features/profile/types/profile_state.dart';
+
 final supabaseClientProvider = Provider<SupabaseClient>((ref) {
   return Supabase.instance.client;
 });
@@ -49,4 +54,22 @@ final loginControllerProvider =
     StateNotifierProvider<LoginController, LoginState>((ref) {
       final usecase = ref.read(loginUsecaseProvider);
       return LoginController(usecase);
+    });
+
+/// ---------------- PROFILE ----------------
+
+final profileRepositoryProvider = Provider<ProfileRepository>((ref) {
+  final client = ref.read(supabaseClientProvider);
+  return ProfileRepository(client: client);
+});
+
+final profileUsecaseProvider = Provider<GetProfileUseCase>((ref) {
+  final repository = ref.read(profileRepositoryProvider);
+  return GetProfileUseCase(repository);
+});
+
+final profileControllerProvider =
+    StateNotifierProvider<ProfileController, ProfileState>((ref) {
+      final usecase = ref.read(profileUsecaseProvider);
+      return ProfileController(useCase: usecase);
     });
