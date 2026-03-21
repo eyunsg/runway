701bffa4389893cbd54e915e11f181aabc3ed8f7
eyunsg <iyunsung423@gmail.com>
2026-03-21 15:12:29 +0900

    refactor(RUNWAY-14): client 테스트 구조 재정의

--- apps/mobile/test/features/get_profile/get_profile_controller_test.dart
+++ apps/mobile/test/features/get_profile/get_profile_controller_test.dart
@@ -1,50 +1,44 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:runway/features/profile/controller/profile_controller.dart';
-import 'package:runway/features/profile/usecase/profile_usecase.dart';
+import 'package:runway/features/get_profile/controller/get_profile_controller.dart';
+import 'package:runway/features/get_profile/usecase/get_profile_usecase.dart';
 
 class MockGetProfileUseCase extends Mock implements GetProfileUseCase {}
 
 void main() {
   late MockGetProfileUseCase mockUseCase;
-  late ProfileController controller;
+  late GetProfileController controller;
 
   setUp(() {
     mockUseCase = MockGetProfileUseCase();
-    controller = ProfileController(useCase: mockUseCase);
+    controller = GetProfileController(useCase: mockUseCase);
   });
 
   test('fetchProfile 성공 시 state에 데이터가 반영되어야 한다', () async {
-    const accessToken = 'test_token';
-
     final mockResponse = {'email': 'test@test.com', 'displayName': 'tester'};
 
-    when(
-      () => mockUseCase.execute(accessToken),
-    ).thenAnswer((_) async => mockResponse);
+    when(() => mockUseCase.execute()).thenAnswer((_) async => mockResponse);
 
-    await controller.fetchProfile(accessToken);
+    await controller.fetchProfile();
 
     expect(controller.state.isLoading, false);
     expect(controller.state.error, null);
     expect(controller.state.email, 'test@test.com');
     expect(controller.state.displayName, 'tester');
 
-    verify(() => mockUseCase.execute(accessToken)).called(1);
+    verify(() => mockUseCase.execute()).called(1);
   });
 
   test('fetchProfile 실패 시 error가 설정되어야 한다', () async {
-    const accessToken = 'test_token';
+    when(() => mockUseCase.execute()).thenThrow(Exception('error'));
 
-    when(() => mockUseCase.execute(accessToken)).thenThrow(Exception('error'));
-
-    await controller.fetchProfile(accessToken);
+    await controller.fetchProfile();
 
     expect(controller.state.isLoading, false);
     expect(controller.state.error, isNotNull);
     expect(controller.state.email, null);
     expect(controller.state.displayName, null);
 
-    verify(() => mockUseCase.execute(accessToken)).called(1);
+    verify(() => mockUseCase.execute()).called(1);
   });
 }
