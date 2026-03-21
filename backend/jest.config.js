775067146275ce5e045d5e26c1d093aef98a4cba
eyunsg <iyunsung423@gmail.com>
2026-03-21 15:15:43 +0900

    refactor(RUNWAY-14): backend 테스트 경로 수정

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
 
-  testMatch: ['<rootDir>/**/*.test.ts'],
+  testMatch: ['<rootDir>/test/**/*.test.ts'],
 
   transform: {
     '^.+\\.ts$': 'ts-jest',
