d04d4251628d387c4afd7c4e5ec2c1a686cbace0
eyunsg <iyunsung423@gmail.com>
2026-04-07 11:04:51 +0900

    Feature/RUNWAY-283 implement get portfolio data integration (#107)
    * feat(RUNWAY-273): 포트폴리오 테이블 생성 및 배포 완료
    
    * feat(RUNWAY-274): 포트폴리오 테이블 RLS 구현 및 배포 완료
    
    * Feature/RUNWAY-278 implement add portfolio data integration (#105)
    
    * feat(RUNWAY-278): provider, controller, usecase, repository 등 구현
    
    * refactor(RUNWAY-278): library prefix & failure extension 정의
    
    * feat(RUNWAY-278): controller, usecase test 구현
    
    * feat(RUNWAY-283): 포트폴리오 조회 기능 구현
    
    * feat(RUNWAY-283): 포트폴리오 조회 유닛 테스트 구현
    
    ---------
    
    Co-authored-by: leeokJ <lucylove2@naver.com>

---
+++ apps/mobile/lib/features/portfolio/controller/get_portfolio_controller.dart
@@ -0,0 +1,65 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:runway/core/error/failure_extension.dart';
+import 'package:runway/features/portfolio/types/get_portfolio_state.dart';
+import 'package:runway/features/portfolio/usecase/get_portfolio_usecase.dart';
+
+class GetPortfolioController extends StateNotifier<GetPortfolioState> {
+  final GetPortfolioUseCase useCase;
+
+  GetPortfolioController({required this.useCase})
+    : super(const GetPortfolioState());
+
+  int _offset = 0;
+  final int _limit = 10;
+
+  Future<void> fetchPortfolio() async {
+    _offset = 0;
+
+    state = state.copyWith(
+      isLoading: true,
+      error: null,
+      portfolios: [],
+      hasMore: true,
+    );
+
+    final result = await useCase.execute(limit: _limit, offset: _offset);
+
+    result.fold(
+      (failure) {
+        state = state.copyWith(isLoading: false, error: failure.toMessage());
+      },
+      (list) {
+        state = state.copyWith(
+          isLoading: false,
+          portfolios: list,
+          hasMore: list.length == _limit,
+        );
+
+        _offset += _limit;
+      },
+    );
+  }
+
+  Future<void> fetchMore() async {
+    if (state.isLoading || !state.hasMore) return;
+
+    state = state.copyWith(isLoading: true, error: null);
+
+    final result = await useCase.execute(limit: _limit, offset: _offset);
+
+    result.fold(
+      (failure) {
+        state = state.copyWith(isLoading: false, error: failure.toMessage());
+      },
+      (list) {
+        state = state.copyWith(
+          isLoading: false,
+          portfolios: [...state.portfolios, ...list], // append
+          hasMore: list.length == _limit,
+        );
+
+        _offset += _limit;
+      },
+    );
+  }
+}
