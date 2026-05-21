8ade91040ea7ba116363204f4e52dc47a6a9060a
eyunsg <iyunsung423@gmail.com>
2026-05-21 15:44:04 +0900

    feat(RUNWAY-446): scr-s-03 구현

--- apps/mobile/lib/features/password_change/page/password_change_temp_screen.dart
+++ apps/mobile/lib/features/password_change/page/password_change_temp_screen.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:runway/core/theme/app_colors.dart';
+import 'package:runway/core/theme/app_typography.dart';
+import 'package:runway/shared/widgets/button.dart';
 import '../../../core/state/async_state.dart';
 import '../../../core/providers.dart';
 
@@ -42,59 +45,162 @@ class _PasswordChangePageState extends ConsumerState<PasswordChangePage> {
     });
 
     return Scaffold(
+      backgroundColor: AppColors.natural.backgroundColors.primary,
+      appBar: AppBar(
+        backgroundColor: AppColors.natural.backgroundColors.primary,
+        elevation: 0,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 16),
+          child: GestureDetector(
+            onTap: () {
+              context.pop();
+            },
+            child: SizedBox(
+              width: 40,
+              height: 40,
+              child: Center(
+                child: Image.asset(
+                  'icons/arrow_left.png',
+                  width: 20,
+                  height: 20,
+                ),
+              ),
+            ),
+          ),
+        ),
+        centerTitle: true,
+        title: Text(
+          '비밀번호 변경',
+          style: AppTypography.heading.h4.copyWith(
+            color: AppColors.natural.textColors.primary,
+          ),
+        ),
+      ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        padding: const EdgeInsets.all(24.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            const SizedBox(height: 24),
-
             TextField(
               controller: _currentPasswordController,
               enabled: state.status != AsyncStatus.loading,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: '현재 비밀번호',
-              ),
               obscureText: true,
+              style: AppTypography.body.m.copyWith(
+                color: AppColors.natural.textColors.primary,
+              ),
+              decoration: InputDecoration(
+                hintText: '현재 비밀번호',
+
+                hintStyle: AppTypography.body.m.copyWith(
+                  color: AppColors.natural.textColors.secondary,
+                ),
+
+                // 기본 상태
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(
+                    color: AppColors.natural.textColors.secondary,
+                    width: 1,
+                  ),
+                ),
+
+                // 포커스 상태
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(
+                    color: AppColors.highlight.light,
+                    width: 1.5,
+                  ),
+                ),
+              ),
               inputFormatters: [
                 FilteringTextInputFormatter.deny(RegExp(r'\s')),
               ],
             ),
-            const SizedBox(height: 16),
+            const SizedBox(height: 24),
 
             TextField(
               controller: _newPasswordController,
               enabled: state.status != AsyncStatus.loading,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
+              obscureText: true,
+              style: AppTypography.body.m.copyWith(
+                color: AppColors.natural.textColors.primary,
+              ),
+              decoration: InputDecoration(
                 hintText: '새 비밀번호',
-              ),
-              obscureText: true,
-              inputFormatters: [
-                FilteringTextInputFormatter.deny(RegExp(r'\s')),
-              ],
-            ),
-            const SizedBox(height: 16),
 
-            TextField(
-              controller: _confirmPasswordController,
-              enabled: state.status != AsyncStatus.loading,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: '새 비밀번호 확인',
+                hintStyle: AppTypography.body.m.copyWith(
+                  color: AppColors.natural.textColors.secondary,
+                ),
+
+                // 기본 상태
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(
+                    color: AppColors.natural.textColors.secondary,
+                    width: 1,
+                  ),
+                ),
+
+                // 포커스 상태
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(
+                    color: AppColors.highlight.light,
+                    width: 1.5,
+                  ),
+                ),
               ),
-              obscureText: true,
               inputFormatters: [
                 FilteringTextInputFormatter.deny(RegExp(r'\s')),
               ],
             ),
             const SizedBox(height: 24),
 
-            state.status == AsyncStatus.loading
-                ? const Center(child: CircularProgressIndicator())
-                : ElevatedButton(
-                    onPressed: () {
+            TextField(
+              controller: _confirmPasswordController,
+              enabled: state.status != AsyncStatus.loading,
+              obscureText: true,
+              style: AppTypography.body.m.copyWith(
+                color: AppColors.natural.textColors.primary,
+              ),
+              decoration: InputDecoration(
+                hintText: '새 비밀번호 확인',
+
+                hintStyle: AppTypography.body.m.copyWith(
+                  color: AppColors.natural.textColors.secondary,
+                ),
+
+                // 기본 상태
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(
+                    color: AppColors.natural.textColors.secondary,
+                    width: 1,
+                  ),
+                ),
+
+                // 포커스 상태
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(
+                    color: AppColors.highlight.light,
+                    width: 1.5,
+                  ),
+                ),
+              ),
+              inputFormatters: [
+                FilteringTextInputFormatter.deny(RegExp(r'\s')),
+              ],
+            ),
+            const SizedBox(height: 24),
+
+            AppButton(
+              text: state.status == AsyncStatus.loading ? '변경 중...' : '비밀번호 변경',
+              variant: ButtonVariant.primary,
+              onPressed: state.status == AsyncStatus.loading
+                  ? () {}
+                  : () {
                       FocusScope.of(context).unfocus();
 
                       ref
@@ -107,11 +213,7 @@ class _PasswordChangePageState extends ConsumerState<PasswordChangePage> {
                                 .trim(),
                           );
                     },
-                    child: const Padding(
-                      padding: EdgeInsets.symmetric(vertical: 16.0),
-                      child: Text('비밀번호 변경'),
-                    ),
-                  ),
+            ),
           ],
         ),
       ),
